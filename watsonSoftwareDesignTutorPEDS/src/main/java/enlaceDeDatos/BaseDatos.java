da21f51de0631829a0c2a48991bf188fa9eb9783
Ernesto Brenes <ernestobrenes96@gmail.com>
2016-10-03 10:57:06 -0600

    Agrego la funcionalidad de acceso a la base de datos
    Acceso a la base de datos redis desde java

--- watsonSoftwareDesignTutorPEDS/src/main/java/enlaceDeDatos/BaseDatos.java
+++ watsonSoftwareDesignTutorPEDS/src/main/java/enlaceDeDatos/BaseDatos.java
@@ -2,15 +2,34 @@ package enlaceDeDatos;
 
 import java.util.*;
 
+import redis.clients.jedis.Jedis;
+
+
 /**
  * 
  */
 public class BaseDatos {
 
+	private Jedis baseDatosRedis;
     /**
      * Default constructor
      */
-    public BaseDatos() {
+    public BaseDatos() {   	
+    	baseDatosRedis = new Jedis("pub-redis-15866.dal-05.1.sl.garantiadata.com",15866);
+    	 baseDatosRedis.auth("pepe1196");
+    }
+    
+    public void insertarDatos(String pClave,String pPregunta, ArrayList<String> pRespuestas)
+    {
+    	for(int i = 0; i<pRespuestas.size();i++)
+    	{
+    		baseDatosRedis.lpush(pClave, pPregunta , pRespuestas.get(i));
+    	}
+    	
+      /*  List<String> list = baseDatosRedis.lrange(pClave, 0 ,100);
+        for(int i=0; i<list.size(); i++) {
+          System.out.println("categoria: "+list.get(i));}*/
+   
     }
 
 }
--- watsonSoftwareDesignTutorPEDS/src/main/java/logicaDePresentacion/Principal.java
+++ watsonSoftwareDesignTutorPEDS/src/main/java/logicaDePresentacion/Principal.java
@@ -1,11 +1,55 @@
 package logicaDePresentacion;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.RedisConnection;
+import com.lambdaworks.redis.RedisURI;
+
+import enlaceDeDatos.BaseDatos;
+import redis.clients.jedis.Jedis;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.print("nanito17");
-		System.out.print("pepe");
+		
+		
+		BaseDatos p = new BaseDatos();
+	    ArrayList<String> pe = new ArrayList<String>();
+	    pe.add("Saprissa");
+	    pe.add("Liga");
+	    pe.add("Cartago");
+	    p.insertarDatos("Equipo", "Futbol", pe);
+        //jedis.lpush("prueba", "p1","tipo algo");
+        //jedis.lpush("prueba", "p1","tipo algo 2");
+        //jedis.lpush("prueba", "p2","Abstraccion");
 
+        
+
+   
+        
+	     /* RedisClient redisClient = new RedisClient(
+	              RedisURI.create("redis://pepe1196@pub-redis-15866.dal-05.1.sl.garantiadata.com:15866"));
+	          RedisConnection<String, String> connection = redisClient.connect();
+	          System.out.println("Connected to Redis using SSL");
+	         
+	          connection.set("key", "pepe");
+
+	          connection.lpush("key", "Hello, Redis!");
+	         
+	          
+	          List<String> list = connection.lrange("key", 0 ,1);
+	          for(int i=0; i<list.size(); i++) {
+	            System.out.println("nombre: "+list.get(i));
+	          }
+	     
+
+	          connection.close();
+	          redisClient.shutdown();*/
+		
+		
+		
 	}
 
 }
